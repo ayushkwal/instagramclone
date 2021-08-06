879335d4db5f16153d8484e9ab8a93c936f36d82
ayush <ayush.kwal@gmail.com>
2021-08-06 22:37:56 +0530

    hey1

--- app.js
+++ app.js
@@ -79,8 +79,7 @@ io.on('connection',socket=>{
 
 
 //Server Listening
-const hostname = '127.0.0.1';
 const port = process.env.PORT||3000;
-    app.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+    app.listen(port,  () => {
+    console.log(`Server running at http://:${port}/`);
   })
