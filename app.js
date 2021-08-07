893eac34ee4bf95ec474809966988f7906e0d86f
ayush <ayush.kwal@gmail.com>
2021-08-07 16:41:00 +0530

    new push3

--- app.js
+++ app.js
@@ -76,6 +76,15 @@ io.on('connection',socket=>{
 })
 
 
+//for production purpose only
+if(process.env.NODE_ENV==="production"){
+  app.use(express.static('client/build'))
+  const path = require('path');
+  app.get("*",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+  })
+}
+
 
 
 //Server Listening
